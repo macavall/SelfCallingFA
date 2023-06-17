4fc520e6c37f2ccad86fba839a4ccd9a5f1e38d9
Matthew Cavallo <macavall@microsoft.com>
2023-06-16 19:44:05 -0500

    Working now

--- timer.cs
+++ timer.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -8,15 +10,26 @@ namespace TemplateHttp
 
     public static class timer
     {
+        public static HttpClient client = new HttpClient();
+
         public static int started = 0;
         [FunctionName("timer")]
-        public static void Run([TimerTrigger("* */5 * * * *", RunOnStartup = true)]TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("*/1 * * * * *")]TimerInfo myTimer, ILogger log)
         {
             if (started == 0)
             {
-                started = 1;
+                //started = 1;
 
                 log.LogInformation($"Kicking off the Function Call to the Http Trigger");
+
+                string selfUri = Environment.GetEnvironmentVariable("WEBSITE_HOSTNAME");
+                selfUri = "https://" + selfUri + "/api/httpex";
+
+                Uri myUri = new(selfUri);
+
+                var msg = new HttpRequestMessage(HttpMethod.Get, selfUri);
+
+                var response = await client.SendAsync(msg);
             }
 
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
